d9569534780eb0cfccb24889141d90b124bfd43c
crabl <rabl.christopher@gmail.com>
2016-02-27 16:39:51 -0700

    remove print

--- tokenizer.py
+++ tokenizer.py
@@ -1,7 +1,6 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from extractor import extract_descriptions
 
-
 bigram_vectorizer = CountVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b', min_df=1)
 analyze = bigram_vectorizer.build_analyzer()
 
@@ -10,4 +9,3 @@ counts = bigram_vectorizer.fit_transform(descriptions.corpus).toarray()
 transformer = TfidfTransformer()
 
 weighted = transformer.fit_transform(counts)
-print weighted.toarray()[0][20]
